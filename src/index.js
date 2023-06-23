a10ebec341170dd0fbf6a5b0ff8a65dabc3c82bb
Serhii Rudenko <svr.phoenix@gmail.com>
2023-06-23 07:48:33 +0300

    Fixed basename

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { store, persistor } from 'redux/store';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <BrowserRouter basename="/phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
